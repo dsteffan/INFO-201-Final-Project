b0b8c1f0d4d557334096763c8a11fc07673c654e
Jessica Yuen <jyuen416@uw.edu>
2017-03-09 01:07:31 -0800

    Add hyperlink to intro

--- ui.r
+++ ui.r
@@ -12,11 +12,9 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
-      h1("Sidebar Panel"),
+      h3("Narrow your search"),
       
-      p("Majors"),
-      
-      selectInput('major.choice', label = "Select Major Category:", choices = majors, selected = 'All'),
+      selectInput('major.choice', label = "Select Major Category:", choices = data$Major_category, selected = 'All'),
       sliderInput('median.range', label = "Median Pay", min = median.range[1], max = median.range[2], value = median.range)
       
       
@@ -33,8 +31,8 @@ ui <- fluidPage(
                                    data, we created separate tabs for each set of information including table of information about each major,
                                     the employment rate, salary, and how useful a college degree is for that major allowing students and parents to learn
                                    more information about certain majors. They can also refine their search by focusing on a specific major category 
-                                   and median salary of those majors."), p("Website to the original data: http://www.census.gov/acs/www/data_documentation/pums_data/"),
-                                  p("Data organized by FiveThirtyEight: https://github.com/fivethirtyeight/data/tree/master/college-majors")),
+                                   and median salary of those majors."), a("Click here to view the original data", href = "http://www.census.gov/acs/www/data_documentation/pums_data/"),
+                                   p(), a("Click here to view the data organized by FiveThirtyEight", href = "https://github.com/fivethirtyeight/data/tree/master/college-majors")),
         
         tabPanel("Major Information", p("Table of Majors"), tableOutput('table')),
         tabPanel("Employment", p("Plot of the majors by employment rate"), plotlyOutput('graph')),
@@ -42,8 +40,7 @@ ui <- fluidPage(
                  plotOutput('histogram', click = 'hist.click'), 
                  textOutput("x_value")),
         
-        tabPanel("College Degree", p("This bar chart takes the data from American Community Survey 2010-2012 Public Use Microdata Series,
-                                            organized by FiveThirtyEight and displays the percentage of jobs requiring college degrees given a major
+        tabPanel("College Degree", p("Percentage of jobs requiring college degrees given a major
                                             category."), 
                  plotOutput('college.jobs', click = 'percent.click'),
                  textOutput("info"))
@@ -53,4 +50,4 @@ ui <- fluidPage(
 )
 
 shinyUI(ui)
-
+?a()
